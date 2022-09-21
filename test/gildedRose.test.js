7074b800f63ebef83e7483e13e867e3f321a448e
Zac <zac@zacmoss.co.uk>
2022-09-21 17:19:48 +0100

    added new tests to test refactored methods

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -15,6 +15,12 @@ describe("Gilded Rose", function () {
     expect(item.quality).toBe(0);
   });
 
+  it("brie: check method returns true", () => {
+    const gildedRose = new Shop();
+    expect(gildedRose.agedBrieCheck(new Item("foo", 1, 1))).toBe(undefined);
+    expect(gildedRose.agedBrieCheck(new Item("Aged Brie", 1, 1))).toBe(true);
+  });
+
   it("brie: sellIn -1 and quality +1", () => {
     const gildedRose = new Shop([new Item("Aged Brie", 1, 1)]);
     const item = gildedRose.updateQuality()[0];
@@ -27,6 +33,18 @@ describe("Gilded Rose", function () {
     const item = gildedRose.updateQuality()[0];
     expect(item.sellIn).toBe(-1);
     expect(item.quality).toBe(2);
+  });
+
+  it("backstages passes: check method returns true", () => {
+    const gildedRose = new Shop();
+    expect(gildedRose.backstagePassesCheck(new Item("foo", 1, 1))).toBe(
+      undefined
+    );
+    expect(
+      gildedRose.backstagePassesCheck(
+        new Item("Backstage passes to a TAFKAL80ETC concert", 1, 1)
+      )
+    ).toBe(true);
   });
 
   it("backstage passes: sellIn -1 and quality + 1", () => {
