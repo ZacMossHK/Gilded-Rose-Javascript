193ae434a4b6322af6afa63390888e9f9f6590b5
Zac <zac@zacmoss.co.uk>
2022-09-21 16:05:58 +0100

    written second brie test

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -21,4 +21,11 @@ describe("Gilded Rose", function () {
     expect(item.sellIn).toBe(0);
     expect(item.quality).toBe(2);
   });
+
+  it("brie: sellIn -1 and quality +2 if sellIn <= 0", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+    const item = gildedRose.updateQuality()[0];
+    expect(item.sellIn).toBe(-1);
+    expect(item.quality).toBe(2);
+  });
 });
