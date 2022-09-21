6142f0e3686134057b882cd345dc8c5726a05065
Zac <zac@zacmoss.co.uk>
2022-09-21 15:52:12 +0100

    updated second test

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -4,7 +4,7 @@ describe("Gilded Rose", function () {
   it("should decrement the sellIn and quality of a normal item by one if sellIn is greater than 0", () => {
     const gildedRose = new Shop([new Item("foo", 1, 1)]);
     const item = gildedRose.updateQuality()[0];
-    expect(items.sellIn).toBe(0);
-    expect(items.sellIn).toBe(0);
+    expect(item.sellIn).toBe(0);
+    expect(item.sellIn).toBe(0);
   });
 });
