d7439d8ca849278f29a74545a235970ff8630f88
Zac <zac@zacmoss.co.uk>
2022-09-21 15:41:43 +0100

    renamed files to camelcase and removed legacy code from updateQuality

--- test/texttestFixture.js
+++ test/texttestFixture.js
@@ -1,5 +1,4 @@
-
-const { Shop, Item } = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gildedRose");
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
@@ -15,13 +14,15 @@ const items = [
   new Item("Conjured Mana Cake", 3, 6),
 ];
 
-const days = Number(process.argv[2]) || 2;
+const days = Number(process.argv[2]) || 10;
 const gildedRose = new Shop(items);
 
 console.log("OMGHAI!");
 for (let day = 0; day < days; day++) {
   console.log(`\n-------- day ${day} --------`);
   console.log("name, sellIn, quality");
-  items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+  items.forEach((item) =>
+    console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
+  );
   gildedRose.updateQuality();
 }
