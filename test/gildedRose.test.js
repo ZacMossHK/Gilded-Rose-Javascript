441cf43da9dc1debcc1045edd6e81d76b63628b9
Zac <zac@zacmoss.co.uk>
2022-09-21 15:48:57 +0100

    expanded on second test

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -3,14 +3,16 @@ const { Shop, Item } = require("../src/gildedRose");
 describe("Gilded Rose", function () {
   it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
-    expect(items[0].sellIn).toBe(0);
-    expect(items[0].quality).toBe(0);
+    const item = gildedRose.updateQuality()[0];
+    expect(item.name).toBe("foo");
+    expect(item.sellIn).toBe(0);
+    expect(item.quality).toBe(0);
   });
 
-  it("should decrement the sellIn and quality of a normal item", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("should decrement the sellIn and quality of a normal item by one if sellIn is greater than 0", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
     const items = gildedRose.updateQuality();
+    expect(items.sellIn).toBe(0);
+    expect(items.sellIn).toBe(0);
   });
 });
