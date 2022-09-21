8d57a0afab6c60085895b0032dd05a14fd88e0f3
Zac <zac@zacmoss.co.uk>
2022-09-21 16:04:36 +0100

    passed first brie test

--- src/gildedRose.js
+++ src/gildedRose.js
@@ -10,8 +10,13 @@ class Shop {
   constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
-    this.items[0].quality -= this.items[0].sellIn > 0 ? 1 : 2;
+    if (this.items[0].name === "Aged Brie") {
+      this.items[0].quality++;
+    } else {
+      this.items[0].quality -= this.items[0].sellIn > 0 ? 1 : 2;
+    }
     this.items[0].sellIn--;
     return this.items;
   }
--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -16,7 +16,7 @@ describe("Gilded Rose", function () {
   });
 
   it("brie: sellIn -1 and quality +1 if sellIn > 0", () => {
-    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 1)]);
     const item = gildedRose.updateQuality()[0];
     expect(item.sellIn).toBe(0);
     expect(item.quality).toBe(2);
