493ca8644b5b40a725ff60136dfa6701f63a137b
Zac <zac@zacmoss.co.uk>
2022-09-22 12:00:21 +0100

    written agedbrie test in checkitem

--- test/checkItem.test.js
+++ test/checkItem.test.js
@@ -8,7 +8,13 @@ describe("CheckItem class", () => {
     checkItem = new CheckItem();
   });
 
-  it("returns normal item", () => {
+  it("returns normalItem if normal item", () => {
     expect(checkItem.getItemToUpdate(new Item("foo", 1, 1))).toBe("normalItem");
+  });
+
+  it("returns agedBrie if Aged Brie", () => {
+    const item = new Item("Aged Brie", 1, 1);
+    expect(checkItem.isAgedBrie(item)).toBe("agedBrie");
+    expect(checkItem.getItemToUpdate(item)).toBe("agedBrie");
   });
 });
