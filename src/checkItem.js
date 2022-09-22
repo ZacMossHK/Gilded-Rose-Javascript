4902d80e1517629d39c77d81e810d8e40b9e633c
Zac <zac@zacmoss.co.uk>
2022-09-22 12:09:42 +0100

    passed backstagepasses check

--- src/checkItem.js
+++ src/checkItem.js
@@ -1,10 +1,17 @@
 class CheckItem {
   getItemToUpdate(item) {
-    return this.isAgedBrie(item) || "normalItem";
+    return (
+      this.isBackstagePasses(item) || this.isAgedBrie(item) || "normalItem"
+    );
   }
 
   isAgedBrie(item) {
     if (item.name === "Aged Brie") return "agedBrie";
+  }
+
+  isBackstagePasses(item) {
+    if (item.name === "Backstage passes to a TAFKAL80ETC concert")
+      return "backstagePasses";
   }
 }
 
