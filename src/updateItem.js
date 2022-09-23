d1121b71a0f3f6302a5884b974c1e842557278fc
Zac <zac@zacmoss.co.uk>
2022-09-23 14:56:42 +0100

    refactored updateItem.backstagePasses

--- src/updateItem.js
+++ src/updateItem.js
@@ -1,3 +1,5 @@
+const Item = require("./item");
+
 class UpdateItem {
   normalItem(item) {
     item.quality -= item.sellIn >= 0 ? 1 : 2;
@@ -10,7 +12,9 @@ class UpdateItem {
   }
 
   backstagePasses(item) {
-    item.quality += item.sellIn >= 10 ? 1 : item.sellIn >= 5 ? 2 : 3;
+    item.quality += 1;
+    if (item.sellIn < 10) item.quality += 1;
+    if (item.sellIn < 5) item.quality += 1;
     if (item.sellIn < 0) item.quality = 0;
     return item;
   }
